60c9910a0f818d10f66a73894ed4406ec9ee1ea8
Junior Aquino <93616016+GitAquino@users.noreply.github.com>
2024-07-20 16:07:28 -0300

    v1.6.2 - Bug Fix
    Ajustes realizados para renderizar em qualquer tipo de tela

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,6 @@ import iconFacebook from '../components/icons8-facebook-48.png';
 import { FcGoogle } from "react-icons/fc";
 import { MdVisibilityOff } from "react-icons/md";
 
-
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -17,12 +16,12 @@ const Login = () => {
   };
 
   return (
-    <div id='TelaInteira'>
+    <div className='TelaInteira'>
       <div className="loginContainer">
         <form onSubmit={handleSubmit} className="formularioLogin">
           <img id='LogoEducpay' src={logo} alt='Logo Educpay' width={130}/>
           <h2>Bem-vindo(a) de volta!</h2>
-          <p id='subtitulo'>Ainda não tem uma conta? <a href='www.google.com' target="_blank" rel="noopener noreferrer">Cadastrar-se</a></p>
+          <p id='subtitulo'>Ainda não tem uma conta? <a id='subtituloLink' href='' target="_blank" rel="noopener noreferrer">Cadastrar-se</a></p>
           <div className="formularioComponentes">
             <label>Email ou Telefone</label>
             <input
@@ -35,32 +34,35 @@ const Login = () => {
           <div className="formularioComponentes">
             <label>Senha</label>
             <div className="inputIconContainer">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              
-            /><MdVisibilityOff className='iconSenha' id='iconSenha'/>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <MdVisibilityOff className='iconSenha' id='iconSenha'/>
             </div>
           </div>
           <button type="submit" className="botaoLogin"><strong>Entrar</strong></button>
-          <p id="esqueciSenha"><a href='www.google.com'>Esqueci a senha</a></p>
+          <p id="esqueciSenha"><a href=''>Esqueci a senha</a></p>
           <p id="entreComOpcoes">
             <span className="linha"></span>
             Ou entre com
             <span className="linha"></span>
           </p>
           <div className="botoesContainer">
-                <button className="botaoGoogle"><FcGoogle className='icon'/>Google</button>
-                <button className="botaoFacebook"><img className='icon' src={iconFacebook} alt='Icon Facebook' width={18}/>Facebook</button>
+            <button className="botaoGoogle">
+              <FcGoogle className='icon'/>Google
+            </button>
+            <button className="botaoFacebook">
+              <img className='icon' src={iconFacebook} alt='Icon Facebook' width={20}/>Facebook
+            </button>
           </div>
-          <p id="Termos">Confira nossos <a href='www.google.com'>Termos e Condições</a></p>
-          <div className='footer'></div>
+          <p id="Termos">Confira nossos <a id="TermosLink" href=''>Termos e Condições</a></p>
         </form>
         <div className='LoginImage'>
-        {/* Aqui você pode adicionar outra imagem, se necessário */}
-      </div>  
+          {/* Aqui você pode adicionar outra imagem, se necessário */}
+        </div>
       </div>
     </div>
   );
